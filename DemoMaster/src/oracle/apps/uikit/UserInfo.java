59d40e6d4bf8ad70c06c693a071f8a1dc70eef2d
lekhili <mohamed.lekhili@virtual7.de>
2017-02-21 20:30:22 +0100

    Login Änderungen

---
+++ DemoMaster/src/oracle/apps/uikit/UserInfo.java
@@ -0,0 +1,42 @@
+package oracle.apps.uikit;
+
+public class UserInfo {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String password;
+    
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
