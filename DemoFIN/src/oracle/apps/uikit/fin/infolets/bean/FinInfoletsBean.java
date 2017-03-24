a8ff45de5d5d93d86d1b4e007f60411cc7490a14
Timo Hahn <tompeez@gmail.com>
2017-03-24 13:47:01 +0100

    removed unused old projects

--- DemoFIN/src/oracle/apps/uikit/fin/infolets/bean/FinInfoletsBean.java
+++
@@ -1,9 +0,0 @@
-package oracle.apps.uikit.fin.infolets.bean;
-
-/*
- * Copyright (c) 2016, 2013, Oracle and/or its affiliates. All rights reserved.
- *
-**/
-
-public class FinInfoletsBean {
-}//FinInfoletsBean
